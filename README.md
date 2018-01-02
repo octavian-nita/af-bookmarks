135cfe4328b89aa4c9896d899f4e5e67c6346680
octavian <octavian.nita@gmail.com>
2018-01-02 17:56:27 +0100

    better, nicer code

--- README.md
+++ README.md
@@ -2,15 +2,23 @@
 
 Simple Node.js "echo" server to inspect http requests ( as well as play with the latest Node and ES201x features ;) ).
 
-### See also
+### See Also
 
-* [**ech** - Echo Entire HTTP Requests in JSON](https://www.npmjs.com/package/ech); ***main** inspiration* and where *kudos* goes to
+* [**ech** - Echo Entire HTTP Requests in JSON](https://www.npmjs.com/package/ech); _main_ inspiration and where _kudos_ goes to
 * [**reflect.py** - A simple echo server to inspect http web requests](https://gist.github.com/huyng/814831)
 * [**echo.py** - Simple echo server written in pure Python](https://gist.github.com/solusipse/6419144)
 
 ---
 
 > Brothers, what we do in life... echoes in eternity.
-> - [Maximus](http://www.imdb.com/title/tt0172495/characters/nm0000128?ref_=tt_cl_t1)
+>
+> * [Maximus](http://www.imdb.com/title/tt0172495/characters/nm0000128?ref_=tt_cl_t1)
 
 [Ah. I've always wanted to say that!](https://getyarn.io/yarn-clip/b1cc7e8e-06fe-493f-a920-85131c927723) ;)
+
+---
+
+### Setting Up a Development Environment
+
+* [Configure Prettier and ESLint in Visual Studio Code](https://www.39digits.com/configure-prettier-and-eslint-in-visual-studio-code/)
+* [Create environment specific configuration for a Node app](https://www.39digits.com/create-environment-specific-config-for-a-node-app/)
--- echs.js
+++ echs.js
@@ -1,38 +1,43 @@
-'use strict';
+const os = require('os');
+const ps = require('process');
+const path = require('path');
+const http = require('http');
+const util = require('util');
 
-const
-  os = require('os'),
-  ps = require('process'),
-  path = require('path'),
-  http = require('http'),
-  util = require('util'),
-  port = ps.env.ECHS_PORT || 3987,
-  server = util.format('%s/%s (%s)', ps.title.charAt(0).toUpperCase() + ps.title.slice(1),
-                       ps.version.replace(/[A-Za-z ]+/g, ''), os.type());
+const port = ps.env.ECHS_PORT || 3987;
+const srvHead = util.format(
+  '%s/%s (%s)',
+  ps.title.charAt(0).toUpperCase() + ps.title.slice(1),
+  ps.version.replace(/[A-Za-z ]+/g, ''),
+  os.type()
+);
+const verbose = util.format(
+  '%s server listening on port %s (pid %s)...',
+  path.basename(__filename, path.extname(__filename)),
+  port,
+  ps.pid
+);
 
 http
   .createServer((req, res) => {
     const message = 'Hello there! Who might you be -- Marian/Venom by any chance ;) ?';
 
     Object.entries({
-
       'Content-Type': 'text/plain; charset=utf-8',
       'Content-Length': Buffer.byteLength(message, 'utf8'),
 
       // see https://stackoverflow.com/a/4881279/272939
-      'Date': new Date().toUTCString(),
-      //? 'Connection': 'close',
-      'Server': server,
+      Date: new Date().toUTCString(),
+      // ? 'Connection': 'close',
+      Server: srvHead,
 
       // No caching
       'Cache-Control': 'no-cache, no-store, must-revalidate, max-age=0', // HTTP 1.1
-      'Pragma': 'no-cache',                                              // HTTP 1.0
-      'Expires': '0'                                                     // Proxies
-
+      Pragma: 'no-cache', // HTTP 1.0
+      Expires: '0' // Proxies
     }).forEach(([headerName, headerValue]) => res.setHeader(headerName, headerValue));
 
     res.statusCode = 200;
     res.end(message);
   })
-  .listen(port, () => console.log('%s server listening on port %s (pid %s)...',
-                                  path.basename(__filename, path.extname(__filename)), port, ps.pid));
+  .listen(port, () => verbose && console.log(verbose));
